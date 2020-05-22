a9d6e27dccd6e5b0d60fb10aa43196e52a6c2f83
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-05-23 01:05:43 +0530

    Create (Submit Notes in Web)application.py

---
+++ Flask/(Submit Notes in Web)application.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_tempalte,request,session
+from flask_session import Session
+
+app=Flask(__name__)
+
+app.config['SESSION_PERMANENT']=False
+app.config['SESSION_TYPE']='filesystem'
+Session(app)
+
+@app.route('/',method=['GET','POST'])
+def index():
+	if session.get('notes') is None:
+		session['notes']=[]
+	if request.method=='POST':
+		note=request.form.get('note')
+		session['notes'].append(note)
+	
+	return render_tempalte('index.html',notes=session['notes'])
