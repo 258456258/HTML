3431c7b6fbadb169827149cf6d98c95d5f774cf9
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-06-25 23:50:43 +0530

    NOTHING CHANGE

---
+++ Django/Project1/testsite/Text Analyzer/Uppercase/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib import admin
+from login import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+   # path('removepunc',views.removepunc,name="removepunc"),
+    path('analyze',views.analyze,name="analyze")
